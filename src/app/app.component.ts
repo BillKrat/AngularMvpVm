3d7754f97d22f0644362b4e1c5d7ae97d80799e8
Bill Kratochvil <53179043+BillKrat@users.noreply.github.com>
2020-01-18 23:29:39 -0600

    Added dynamic components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+  <h1>{{ title }} app is running!</h1>
+  <app-dynamic-container></app-dynamic-container>
+  <router-outlet></router-outlet>
+  `,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DynamicComponent } from './Poc/dynamic/dynamic.component';
+import { DynamicContainerComponent } from './Poc/dynamic-container/dynamic-container.component';
+import { DynamicHostDirective } from './Poc/dynamic-host.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DynamicComponent,
+    DynamicContainerComponent,
+    DynamicHostDirective
   ],
   imports: [
     BrowserModule,
