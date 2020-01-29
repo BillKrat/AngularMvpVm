977d79a0b34d3212d9a67baa264b8ad502229457
Bill Kratochvil <53179043+BillKrat@users.noreply.github.com>
2020-01-29 16:53:04 -0600

    Utility service - has guid function

---
+++ src/app/services/util.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UtilService {
+
+  constructor() { }
+
+  guid() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
+  }
+
+}
